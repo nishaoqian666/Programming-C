9b7acc9490249a920ddb6ae0c45c94d1f4a57600
nishaoqian <nishaoqian666@gmail.com>
2021-03-02 22:52:50 +0300

    1-2

---
+++ 1-2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    double t, v, v1, S;
+
+    cout << "Please input t,v,v1 = ";/*Введите время движения t,
+    скорость v в стоячей воде, скорость течения реки v1*/
+    while (!(cin >> t) || !(cin >> v) || !(cin >> v1)) { //Проверить правильность ввода
+        cout << "error\n";
+        cin.clear();
+        while (cin.get() != '\n') {
+            continue;
+        }
+    }
+    cin.get();
+    S = t * (v - v1);//Вычислить путь, пройденный лодкой по течению
+    cout << "S = " << S << endl;
+    return 0;
+}
