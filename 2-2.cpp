8f560b3513f2e41d5947e37ae1539eded2ea2185
nishaoqian <nishaoqian666@gmail.com>
2021-03-02 22:54:53 +0300

    2-2

---
+++ 2-2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+#define PI 3.1415923
+int main()
+{
+	double a = 1.36;
+	double x, y;
+	cout << "Please input x : ";
+	while (!(cin >> x)) {  //Проверить правильность ввода
+		cout << "error\n";
+		cin.clear();
+		while (cin.get() != '\n') {
+			continue;
+		}
+	}
+	cin.get();
+
+	if (x < 0.5) {
+		y = PI / 2 + a * x - exp(a * x);
+	}
+	else {
+		y = pow(x, 3) * a + 28;
+	}
+
+	cout << "y = : " << y;
+	return 0;
+}
