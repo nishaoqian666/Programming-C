a9069f4ca00f172bdc1637ddb55a911a40561eef
nishaoqian <nishaoqian666@gmail.com>
2021-03-02 22:55:40 +0300

    3-1

---
+++ 3-1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	double x, y;
+	x = 0;
+	while (x <= 1) {
+		y = sqrt(1 - x) - tan(x);
+		cout << "x = " << x << endl;
+		cout << "y = " << y << endl;
+		x = x + 0.1;
+	}
+	return 0;
+}
+
